7c258d1592deb0e1c8acd1770a8e44409bbd65a9
Kristiyan Petrov <kristiyan93@gmail.com>
2025-05-11 18:38:51 +0300

    add footer component

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,28 +1,21 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Inter } from "next/font/google";
+import Footer from "@/components/Footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Google Clone",
-  description: "An open source Google clone built with Next.js and Tailwind CSS.",
+  description:
+    "An open source Google clone built with Next.js and Tailwind CSS.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={inter.className}>
         {children}
+        <Footer />
       </body>
     </html>
   );
---
+++ src/components/CountryLookup.jsx
@@ -0,0 +1,25 @@
+'use client';
+import React, { useEffect } from 'react'
+import { useState } from 'react';
+
+export default function CountryLookup() {
+const [country, setCountry] = useState('United States');
+
+useEffect(() => {
+    const getCountry = async() => {
+        const response = await fetch(`https://extreme-ip-lookup.com/json/?key=${process.env.NEXT_PUBLIC_IP_API_KEY}`)
+        .then((res)=>res.json)
+        .then((data)=> data.country);
+        console.log(response)
+        if(!response) return;
+        setCountry(response);
+
+    };
+    getCountry();
+},[]);
+    return (
+    <div>
+    {country}
+    </div>
+  )
+}
