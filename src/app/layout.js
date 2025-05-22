3082397c322acba7621524ceb1381a38d5ec282e
Kristiyan Petrov <kristiyan93@gmail.com>
2025-05-22 18:19:04 +0300

    create search result component

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,8 @@
 import "./globals.css";
-import { Inter } from "next/font/google";
+
 import Footer from "@/components/Footer";
 
-const inter = Inter({ subsets: ["latin"] });
+
 
 export const metadata = {
   title: "Google Clone",
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className='relative min-h-screen'>
         {children}
         <Footer />
       </body>
