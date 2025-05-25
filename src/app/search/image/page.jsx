618d8df46331bd031620afb1b8daf3b921e76bf0
Kristiyan Petrov <kristiyan93@gmail.com>
2025-05-25 15:13:53 +0300

    add loading effects for web and image pages

--- src/app/search/image/page.jsx
+++ src/app/search/image/page.jsx
@@ -5,6 +5,7 @@ import ImageSearchResults from '@/components/ImageSearchResults';
 export default async function ImageSearchPage({searchParams}) {
   const searchTerm = searchParams?.searchTerm || '';
   const startIndex = searchParams?.start || '1';
+  await new Promise ((resolve)=> setTimeout(resolve, 1000));
   const response = await fetch (`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchTerm}&searchType=image&start=${startIndex}`);
   if (!response.ok) throw new Error('Something went wrong');  
   const data = await response.json();
--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Link from 'next/link';
 import WebSearchResults from '@/components/WebSearchResults';
+import { resolve } from 'styled-jsx/css';
 
 export default async function WebSearchPage({searchParams}) {
   const searchTerm = searchParams?.searchTerm || '';
   const startIndex = searchParams?.start || '1';
+  await new Promise ((resolve)=> setTimeout(resolve, 1000));
   const response = await fetch (`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchTerm}&start=${startIndex}`);
   if (!response.ok) throw new Error('Something went wrong');  
   const data = await response.json();
